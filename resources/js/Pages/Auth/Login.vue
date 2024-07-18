c2b67e9fa801afb9718f62bb3504346aa58bf26a
Nero <amacaneil@gmail.com>
2024-07-18 23:20:36 +0800

    fixed logo in login page and added rider dashboard

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -37,7 +37,7 @@ const login = () => {
     <div
         class="flex flex-col bg-accent text-text py-8 px-4 md:px-8 mx-auto max-w-[1900px]"
     >
-        <div class="mb-10 w-12">
+        <div class="w-12 mb-10">
             <a href="/" class="flex items-center gap-2 hover:text-primary">
                 <i class="uil uil-backspace text-h2"> </i>
                 Back
@@ -48,24 +48,28 @@ const login = () => {
             class="flex-row p-24 items-center justify-center mx-auto max-w-[1500px]"
         >
             <div class="flex gap-2 justify-center max-w-[1500px]">
-                <p>[logo]</p>
-                <p class="text-primary text-h2 mb-10">Meals on Wheels</p>
+                <img
+                    class="w-32 h-auto md-custom:w-48"
+                    src="../../../assets/logo.png"
+                    alt="logo"
+                />
+                <p class="mb-10 text-primary text-h2">Meals on Wheels</p>
             </div>
             <div>
-                <p class="text-center font-medium my-2 text-h3">
+                <p class="my-2 font-medium text-center text-h3">
                     Log in to your account
                 </p>
                 <p
-                    class="text-center font-base mb-8 text-smaller text-gray-600"
+                    class="mb-8 text-center text-gray-600 font-base text-smaller"
                 >
                     Welcome back! We are so pleased to see you again :D
                 </p>
             </div>
             <div>
-                <form 
+                <form
                     @submit.prevent="login"
                     class="flex flex-col gap-6 max-w-[1500px]"
-                    >
+                >
                     <div class="flex flex-col gap-3 max-w-[1500px]">
                         <Input
                             type="email"
@@ -85,7 +89,7 @@ const login = () => {
                     </div>
 
                     <button
-                        class="bg-primary hover:bg-secondary text-white text-base py-2 px-4 rounded"
+                        class="px-4 py-2 text-base text-white rounded bg-primary hover:bg-secondary"
                         type="submit"
                     >
                         Login
@@ -93,9 +97,9 @@ const login = () => {
                 </form>
             </div>
             <div>
-                <p class="text-center mt-4 text-smaller text-gray-600">
+                <p class="mt-4 text-center text-gray-600 text-smaller">
                     Don't have an account?
-                    <a href="register" class="hover:text-primary underline"
+                    <a href="register" class="underline hover:text-primary"
                         >Register Now!</a
                     >
                 </p>
---
+++ resources/js/SharedComponent/OrderTab.vue
@@ -0,0 +1,35 @@
+<template>
+    <div
+        class="flex w-full p-5 m-10 text-center rounded-md shadow cursor-pointer bg-slate-500"
+    >
+    <h1>orders here</h1>
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <div
+                v-for="order in orders"
+                :key="menu.id"
+                class="overflow-hidden bg-white rounded-lg shadow-md"
+            >
+                <div class="p-4">
+                    <h3 class="mb-2 text-lg font-semibold">
+                        {{ order.menu_id }}
+                    </h3>
+                    <p class="text-gray-600">{{ order.status }}</p>
+                </div>
+            </div>
+        </div>
+        <div class="flex rounded-sm justify-self-end bg-primary text-secondary-accent">
+                <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        orders: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
